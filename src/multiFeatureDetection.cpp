80e755fe164e57a3aa5e806fe54bcfaea6056b25
glenn <cyertai@philen.com>
2021-04-22 18:50:37 -0400

    working in single thread

--- src/multiFeatureDetection.cpp
+++ src/multiFeatureDetection.cpp
@@ -44,17 +44,54 @@ int main(int argc, char** argv )
 		}
 		out_frame = in_frame.clone();
 
+		cv::Mat white_mask;
+		cv::Mat gs_frame;
+
+		white_mask.create(in_frame.size(),in_frame.type());
+		cv::cvtColor(in_frame,gs_frame,cv::COLOR_BGR2GRAY);
+
 		//SIFT
 	    std::vector<cv::KeyPoint> keypoints = SIFTonFrame(in_frame);
-		cv::drawKeypoints(out_frame,keypoints,out_frame);
 
 		//corner detection
+		cv::Mat harris_dst = cv::Mat::zeros(gs_frame.size(),CV_32FC1);
+		cv::cornerHarris(gs_frame,harris_dst,3,3,0.04);
+
+			//normalize
+		cv::Mat dst_norm;
+		cv::Mat dst_norm_scaled;
+		cv::normalize(harris_dst, dst_norm, 0,255,cv::NORM_MINMAX, CV_32FC1,cv::Mat());
+		cv::convertScaleAbs(dst_norm,dst_norm_scaled);
 
 
+			//draw circle
+
+
+
+		//canny
+
+		cv::Mat detected_edges;
+		white_mask = cv::Scalar::all(255);
+		cv::blur(gs_frame,detected_edges,cv::Size(3,3));
+		cv::Canny(detected_edges, detected_edges,50,150,3);
+
+
+		//apply changes
+		white_mask.copyTo(out_frame,detected_edges);
+		cv::drawKeypoints(out_frame,keypoints,out_frame);
+
+		//draw harris circles
+		for(int i = 0; i < dst_norm.rows; i++) {
+			for(int j = 0; j < dst_norm.cols; j++) {
+				if((int)dst_norm.at<float>(i,j) > 200) {
+					cv::circle(out_frame,cv::Point(j,i),30,cv::Scalar(0),8,8,0);
+				}
+			}
+		}
 
 		//output
 		cv::imshow(windowName,out_frame);
-		cv::waitKey(10); //delay TODO remove & use timer
+		cv::waitKey(100); //delay TODO remove & use timer
 	}
 }
 
